d5d7007c3a05e9b90afdb85b929d4d38d23db6fb
leonardo <leonardo.rl30267@gmail.com>
2026-02-23 21:48:29 -0300

    commit no pycharm

---
+++ main.py
@@ -0,0 +1,9 @@
+import time
+import numpy
+import pygame
+
+start_time = time.time()
+lista = [0] * 100
+end_time = time.time()
+elapsed_time = end_time - start_time
+print(f'{elapsed_time} seconds')
